de2c20684c9c74b070bc1053041c0a21786c521f
nXqd <nxqd.inbox+rhacker@gmail.com>
2015-03-22 05:10:29 +0000

    update production configuration

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,10 +1,27 @@
+var webpack = require('webpack');
+
 module.exports = {
   cache: true,
+  devtool: 'source-map',
   entry: './js/main.js',
   output: {
       path: __dirname + "/dist",
-      filename: 'main.js'
+      filename: 'main.js',
+      publicPath: '/'
   },
+  plugins: [
+      new webpack.optimize.OccurenceOrderPlugin(),
+      new webpack.DefinePlugin({
+          'process.env': {
+              'NODE_ENV': JSON.stringify('production')
+          }
+      }),
+      new webpack.optimize.UglifyJsPlugin({
+          compressor: {
+              warnings: false
+          }
+      })
+  ],
   module: {
     loaders: [
       {test: /\.js$/, loader: 'jsx-loader'},
